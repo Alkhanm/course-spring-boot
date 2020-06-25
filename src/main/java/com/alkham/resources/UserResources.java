0b8531492b7bda9e9d9aa63446b91ff30bf7e21f
Alkhanm <joas2017sousa@hotmail.com>
2020-06-25 12:29:30 -0300

    Entidade User e seu resource

---
+++ src/main/java/com/alkham/resources/UserResources.java
@@ -0,0 +1,23 @@
+package com.alkham.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alkham.entities.User;
+
+@RestController //Define que essa classe é reponsavel por retornar recursos Rest
+@RequestMapping(value = "/users") //Define sobre quais recursos essa classe está apta a controlar
+public class UserResources {
+	
+	/* ReponseEntity<T> = Um tipo especial Spring responsavel por retornar respostas de requisições Web
+	 * É um tipo gênerico, assim é necessario espeficar entre <> qual tipo especifico ele podera retornar*/
+	@GetMapping
+	public ResponseEntity<User> finAll(){
+		User u = new User(2L, "Maria", "Maria@gmail.com", "fsdfsdf", "fsdf");
+		
+		return ResponseEntity.ok().body(u);
+	}
+
+}
