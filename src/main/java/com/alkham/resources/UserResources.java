e6e44befabef59d6e5d9f174c8841b35f953c086
Alkhanm <joas2017sousa@hotmail.com>
2020-06-26 15:13:42 -0300

    User delete

--- src/main/java/com/alkham/resources/UserResources.java
+++ src/main/java/com/alkham/resources/UserResources.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,6 +50,14 @@ public class UserResources {
 		
 		return ResponseEntity.created(uri).body(obj);
 		/* Para ser mais semantico, ao criar uma novo elemento devesse usar o created(uri), passando o URI desse elemento criado. */
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build(); //Já que essa função não retorna nada.
 	}
 
 }
